4624f5f7bdb65a424cc7da81fc81e48ac7777f9c
Haikal <haikalhakim530@gmail.com>
2026-05-22 13:37:35 +0700

    fix(script): add automatically detected

--- eww/scripts/sysinfo.sh
+++ eww/scripts/sysinfo.sh
@@ -2,7 +2,8 @@
 CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf "%.0f", usage}')
 RAM_PERCENT=$(free | awk 'NR==2{printf "%.0f", $3/$2*100}')
 DISK_PERCENT=$(df / | awk 'NR==2{printf "%.0f", $3/$4*100}')
-BATTERY=$(cat /sys/class/power_supply/BAT0/capacity)
-BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
+BATTERY_PATH=$(ls /sys/class/power_supply/ | grep -i bat | head -1)
+BATTERY=$(cat /sys/class/power_supply/$BATTERY_PATH/capacity 2>/dev/null || echo 0)
+BATTERY_STATUS=$(cat /sys/class/power_supply/$BATTERY_PATH/status 2>/dev/null || echo "Unknown")
 
 echo "{\"cpu\": $CPU, \"ram_percent\": $RAM_PERCENT, \"disk_percent\": $DISK_PERCENT, \"battery\": $BATTERY, \"battery_status\": \"$BATTERY_STATUS\"}"
